285070f4388d85d73538c7339f3461e96b796e40
JohnWeeks1 <jonathanweeks007@gmail.com>
2023-03-30 15:33:42 +0700

    first commit

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,7 +3,8 @@ import axios from "axios"
 export default defineNuxtPlugin((NuxtApp) => {
 
     axios.defaults.withCredentials = true;
-    axios.defaults.baseURL = 'http://localhost:8000'
+    // axios.defaults.baseURL = 'http://localhost:8000'
+    axios.defaults.baseURL = 'http://api.johntest.site'
 
     return {
         provide: { 
