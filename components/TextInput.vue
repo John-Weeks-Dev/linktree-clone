829271bde984d614c2c13eb3d858a51e1de967a5
JohnWeeks1 <jonathanweeks007@gmail.com>
2023-04-01 16:09:27 +0700

    added </client-only> in TextInput.vue

--- components/TextInput.vue
+++ components/TextInput.vue
@@ -1,29 +1,31 @@
 <template>
     <div>
-        <input 
-            :id="placeholder"
-            :placeholder="placeholder"
-            :maxlength="String(max)"
-            class="
-                w-full
-                bg-[#EFF0EB]
-                text-gray-800
-                border-2
-                text-sm
-                border-[#EFF0EB]
-                rounded-xl
-                py-3.5
-                px-3
-                placeholder-gray-500
-                focus:outline-none
-            " 
-            @focus="isFocused = true"
-            @blur="isFocused = false"
-            :class="isFocused ? 'border-gray-900' : ''"
-            :type="inputType"
-            v-model="inputComputed"
-            autocomplete="off"
-        >
+        <client-only>
+            <input 
+                :id="placeholder"
+                :placeholder="placeholder"
+                :maxlength="String(max)"
+                class="
+                    w-full
+                    bg-[#EFF0EB]
+                    text-gray-800
+                    border-2
+                    text-sm
+                    border-[#EFF0EB]
+                    rounded-xl
+                    py-3.5
+                    px-3
+                    placeholder-gray-500
+                    focus:outline-none
+                " 
+                @focus="isFocused = true"
+                @blur="isFocused = false"
+                :class="isFocused ? 'border-gray-900' : ''"
+                :type="inputType"
+                v-model="inputComputed"
+                autocomplete="off"
+            >
+        </client-only>
         <span v-if="error" class="text-red-500 text-[14px] font-semibold">
             {{ error }}
         </span>
